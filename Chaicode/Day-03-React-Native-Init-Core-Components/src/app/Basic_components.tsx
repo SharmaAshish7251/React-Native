1d543759e0d24ef999cf2e36e5ab04307fc469c8
Ashish Sharma <59969204+SharmaAshish7251@users.noreply.github.com>
2026-05-28 19:15:26 +0530

    Add Assignment component with email and password input fields, social login buttons, and styling

--- Chaicode/Day-03-React-Native-Init-Core-Components/src/app/Basic_components.tsx
+++ Chaicode/Day-03-React-Native-Init-Core-Components/src/app/Basic_components.tsx
@@ -1,11 +1,37 @@
 import { useState } from "react";
-import { Image, Text, TextInput, View, Pressable } from "react-native";
+import {
+  FlatList,
+  Image,
+  Button,
+  Switch,
+  Text,
+  TextInput,
+  View,
+  Pressable,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 
 export default function BasicComponents() {
   const [name, setName] = useState("");
+  const items = Array.from({ length: 6 }, (_, i) => `Item ${i + 1}`);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const users = [
+    { id: "1", name: "Ashish Sharma", role: "Develop" },
+    { id: "2", name: "Ashu Sharma", role: "Digital Marketing" },
+    { id: "3", name: "Pawan Saxena", role: "Full Stack Developer" },
+    { id: "4", name: "Gvoind Kumar Thakur", role: "HR" },
+    { id: "5", name: "Avnish Kumar Ram", role: "Sales Manager" },
+    { id: "6", name: "Rahul Kumar Sharma", role: "Accountant" },
+  ];
 
   return (
-    <View>
+    <ScrollView
+      style={{
+        backgroundColor: "grey",
+      }}
+    >
       <Text numberOfLines={3}>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora quas
         ratione odit, quisquam assumenda, quidem quibusdam, quam voluptatibus
@@ -68,6 +94,87 @@ export default function BasicComponents() {
           pressed ? <Text> Pressing </Text> : <Text>Press Me</Text>
         }
       </Pressable>
-    </View>
+
+      {items.map((item) => (
+        <Text
+          key={item}
+          style={{
+            marginTop: 6,
+            flex: 3,
+            width: 100,
+            borderRadius: 6,
+            padding: 12,
+            backgroundColor: "white",
+            color: "black",
+          }}
+        >
+          {item}
+        </Text>
+      ))}
+
+      <Button
+        title="Press Me"
+        color="green"
+        onPress={() => alert("Button Pressed")}
+      />
+
+      <Switch
+        value={isDarkMode}
+        onValueChange={setIsDarkMode}
+        trackColor={{ false: "white", true: "white" }}
+        thumbColor={"grey"}
+      />
+
+      <Pressable
+        onPress={() => alert("Pressable Button Pressed")}
+        style={({ pressed }) => ({
+          backgroundColor: pressed ? "purple" : "dark-purple",
+        })}
+        // hitslop used for mini components which not easy to click if we click around that it will run works on pressable component
+        hitSlop={{
+          top: 10,
+          bottom: 10,
+          left: 20,
+          right: 20,
+        }}
+      >
+        {({ pressed }) =>
+          pressed ? <Text>Pressable...</Text> : <Text>Button Pressed</Text>
+        }
+      </Pressable>
+
+      <FlatList
+        style={{
+          backgroundColor: "pink",
+        }}
+        data={users}
+        horizontal // enable horizontal scroll
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <>
+            <Text>{item.id}</Text>
+            <Text>{item.name}</Text>
+            <Text>{item.role}</Text>
+          </>
+        )}
+        contentContainerStyle={{ padding: 16, backgroundColor: "orange" }}
+        ItemSeparatorComponent={() => (
+          <View
+            style={{
+              height: 1,
+              backgroundColor: "green",
+            }}
+          />
+        )}
+      />
+
+        <KeyboardAvoidingView
+              behavior={Platform.OS === "ios" ? "padding" : "height"}
+            >
+              <TextInput placeholder="Enter your Name" />
+              <TextInput placeholder="Enter your Email" />
+              <Button onPress={() => {}} title="Login" />
+            </KeyboardAvoidingView>
+    </ScrollView>
   );
 }
--- Chaicode/Day-03-React-Native-Init-Core-Components/src/app/index.tsx
+++ Chaicode/Day-03-React-Native-Init-Core-Components/src/app/index.tsx
@@ -1,48 +1,17 @@
-import { useState } from "react";
-import { Button, ScrollView, StyleSheet, Switch, Text } from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
+import Assignment from "./Assignment";
 
 const HomeScreen = () => {
-  const items = Array.from({ length: 6 }, (_, i) => `Item ${i + 1}`);
-  const [isDarkMode, setIsDarkMode] = useState(false);
   return (
     <ScrollView
       style={{
-        backgroundColor: "grey",
+        backgroundColor: "white",
       }}
     >
-      {items.map((item) => (
-        <Text
-          key={item}
-          style={{
-            marginTop: 6,
-            flex: 3,
-            width: 100,
-            borderRadius: 6,
-            padding: 12,
-            backgroundColor: "white",
-            color: "black",
-          }}
-        >
-          {item}
-        </Text>
-      ))}
-
-      <Button
-        title="Press Me"
-        color="green"
-        onPress={() => alert("Button Pressed")}
-      />
-
-      <Switch
-        value={isDarkMode}
-        onValueChange={setIsDarkMode}
-        trackColor={{ false: "white", true: "white" }}
-        thumbColor={"grey"}
-      />
-
-      
+      {/* <BasicComponents /> */}
+      <Assignment />
     </ScrollView>
-  );
+  );   
 };
 
 export default HomeScreen;
