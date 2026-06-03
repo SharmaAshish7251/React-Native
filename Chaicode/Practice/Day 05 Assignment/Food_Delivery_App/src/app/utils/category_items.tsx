883267ce6c88d83bc4c4c5c537b5ae263624221e
Ashish Sharma <59969204+SharmaAshish7251@users.noreply.github.com>
2026-06-04 03:44:20 +0530

    Move Practice folder to root level

--- Chaicode/Practice/Day 05 Assignment/Food_Delivery_App/src/app/utils/category_items.tsx
+++
@@ -1,9 +0,0 @@
-const categoryItems = [
-  { id: "1", title: "Pizza" },
-  { id: "2", title: "Burger" },
-  { id: "3", title: "Sushi" },
-  { id: "4", title: "Healthy" },
-  { id: "5", title: "Dessert" },
-];
-
-export default categoryItems;
