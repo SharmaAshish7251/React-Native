2198283cbd507dcac978be9061e06148ea17bb2a
Ashish Sharma <Sharmaashish7251@gmail.com>
2026-06-30 23:59:37 +0530

    Add Day-09-Sensors-and-Motions code

---
+++ Chaicode/Day-09-Sensors-and-Motions/accelerometer/src/hooks/use-accelerometer.ts
@@ -0,0 +1,27 @@
+import { Accelerometer } from "expo-sensors";
+import { useEffect, useState } from "react";
+
+
+export function useAccelerometer() {
+    // state variable
+    const [available, setAvailable] = useState<boolean | null>(null);
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
+    const [z, setZ] = useState(0);
+
+    useEffect(() => {
+        let subscription;
+        (async () => {
+            //  this will check sensor availablity
+            const isAvailable = await Accelerometer.isAvailableAsync();
+            setAvailable(isAvailable);
+            if (!available) return;
+
+            // to get sensor triggered
+            Accelerometer.setUpdateInterval(100);
+        })()
+
+
+    }, [])
+    return {};
+}
